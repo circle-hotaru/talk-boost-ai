8b3f57861d63e3e1135c4e85be37e30774fa1605
Target_right <1402773991@qq.com>
2023-04-06 22:30:16 +0800

    feat:add speech-ai

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,90 +1,100 @@
-import { useEffect, useState, KeyboardEventHandler } from 'react'
-import TextareaAutosize from 'react-textarea-autosize'
-import { sendRequest } from '~/apis/openai'
+import { useEffect, useState, KeyboardEventHandler } from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
+import { sendRequest } from '~/apis/openai';
 import SpeechRecognition, {
   useSpeechRecognition,
-} from 'react-speech-recognition'
+} from 'react-speech-recognition';
+import { useSpeechSynthesis } from 'react-speech-kit';
 
 const Content = () => {
-  const [sendFlag, setSendFlag] = useState<boolean>(false)
-  const [input, setInput] = useState<string>('')
-  const [messages, setMessages] = useState<any[]>([])
-  const [response, setResponse] = useState<string>('')
-  const [recordFlag, setRecordFlag] = useState<boolean>(false)
+  const [sendFlag, setSendFlag] = useState<boolean>(false);
+  const [input, setInput] = useState<string>('');
+  const [messages, setMessages] = useState<any[]>([]);
+  const [response, setResponse] = useState<string>('');
+  const [recordFlag, setRecordFlag] = useState<boolean>(false);
   const {
     transcript,
     listening,
     resetTranscript,
     browserSupportsSpeechRecognition,
-  } = useSpeechRecognition()
-
+  } = useSpeechRecognition();
+  const { speak } = useSpeechSynthesis();
   if (!browserSupportsSpeechRecognition) {
-    console.log("Browser doesn't support speech recognition.")
+    console.log("Browser doesn't support speech recognition.");
   }
 
   const handleSend = () => {
-    const input_json = { role: 'user', content: input }
-    setMessages((prevMessages) => [...prevMessages, input_json])
-    setInput('')
-    setSendFlag(!sendFlag)
-  }
+    const input_json = { role: 'user', content: input };
+    setMessages((prevMessages) => [...prevMessages, input_json]);
+    setInput('');
+    setSendFlag(!sendFlag);
+  };
 
   const handleRecord = () => {
     if (!recordFlag) {
-      SpeechRecognition.startListening()
+      SpeechRecognition.startListening();
     } else {
-      SpeechRecognition.stopListening()
-      setInput(transcript)
+      SpeechRecognition.stopListening();
+      setInput(transcript);
     }
-    setRecordFlag(!recordFlag)
-  }
+    setRecordFlag(!recordFlag);
+  };
 
   const handleKeyDown: KeyboardEventHandler<HTMLTextAreaElement> = (event) => {
     if (event.key === 'Enter' && !event.shiftKey) {
-      event.preventDefault()
-      if (input.length === 0) return
-      handleSend()
+      event.preventDefault();
+      if (input.length === 0) return;
+      handleSend();
     } else if (event.key === 'Enter' && event.shiftKey) {
-      event.preventDefault()
-      setInput(input + '\n')
+      event.preventDefault();
+      setInput(input + '\n');
     }
-  }
-
+  };
+  const handleReturns = () => {
+    if (response) {
+      speak({ text: response });
+    } else {
+      speak({ text: 'Please start chatting with me' });
+    }
+  };
   useEffect(() => {
     if (response.length !== 0 && response !== 'undefined') {
       setMessages((prevMessages) => [
         ...prevMessages,
         { role: 'assistant', content: response },
-      ])
+      ]);
+      setTimeout(() => {
+        speak({ text: response });
+      }, 1000);
     }
-  }, [response])
+  }, [response]);
 
   useEffect(() => {
     if (messages.length > 0) {
-      let messagesToSent = messages
+      let messagesToSent = messages;
       messagesToSent.unshift({
         role: 'system',
         content:
           'You are an English teacher, please help me practice daily English communication. If I make any mistakes, please point them out and correct them.',
-      })
+      });
       sendRequest(messagesToSent, (data: any) => {
         if (data) {
-          setResponse(data.choices[0].message.content)
-          console.log('Response: ' + data.choices[0].message.content)
+          setResponse(data.choices[0].message.content);
+          console.log('Response: ' + data.choices[0].message.content);
         }
       }).catch((err) => {
-        console.log(err)
-      })
+        console.log(err);
+      });
     }
-  }, [sendFlag])
+  }, [sendFlag]);
 
   useEffect(() => {
     if (!listening) {
-      SpeechRecognition.stopListening()
-      setInput(transcript)
-      setRecordFlag(!recordFlag)
+      SpeechRecognition.stopListening();
+      setInput(transcript);
+      setRecordFlag(!recordFlag);
     }
-  }, [listening])
+  }, [listening]);
 
   return (
     <div className="w-full max-w-3xl flex flex-1 flex-col justify-end items-center mt-4">
@@ -117,9 +127,15 @@ const Content = () => {
         >
           Send
         </button>
+        <button
+          onClick={handleReturns}
+          className="border-2 font-bold py-2 px-4 rounded-lg"
+        >
+          Returns
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
