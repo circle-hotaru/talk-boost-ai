7676246615c852c51b20a5a2ba86e0f9a9bffcf0
Target_right <1402773991@qq.com>
2023-05-03 16:15:23 +0800

    feat: change config name

--- src/apis/newTTS.ts
+++ src/apis/newTTS.ts
@@ -8,19 +8,16 @@ import {
 import { useState } from 'react'
 
 const getSpeakToTextApi = () => {
-  const speechConfig = SpeechConfig.fromSubscription(APP_SECRET, APP_REGION)
+  const speechConfig = SpeechConfig.fromSubscription(AZURE_SECRET, AZURE_REGION)
   console.log('speechConfig', speechConfig)
   const audioConfig = AudioConfig.fromDefaultMicrophoneInput()
   const recognizer = new SpeechRecognizer(speechConfig, audioConfig)
   return recognizer
 }
 const getTextToSpeakApi = () => {
-  const speechConfig = SpeechConfig.fromSubscription(APP_SECRET, APP_REGION)
+  const speechConfig = SpeechConfig.fromSubscription(AZURE_SECRET, AZURE_REGION)
   const speechSynthesizer = new SpeechSynthesizer(speechConfig)
   return speechSynthesizer
 }
-const getReturnsTTSApi = () => {
-  let synth = getTextToSpeakApi()
-  synth.SetOutputToDefaultAudioDevice()
-}
+
 export { getSpeakToTextApi, getTextToSpeakApi }
