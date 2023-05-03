6cedf5014110d47e5378b21a18e16cee139ac698
Target_right <1402773991@qq.com>
2023-05-03 17:04:14 +0800

    feat: env variables

--- src/apis/newTTS.ts
+++ src/apis/newTTS.ts
@@ -8,14 +8,20 @@ import {
 import { useState } from 'react'
 
 const getSpeakToTextApi = () => {
-  const speechConfig = SpeechConfig.fromSubscription(AZURE_SECRET, AZURE_REGION)
+  const speechConfig = SpeechConfig.fromSubscription(
+    process.env.VITE_AZURE_SECRET,
+    process.env.VITE_AZURE_REGION,
+  )
   console.log('speechConfig', speechConfig)
   const audioConfig = AudioConfig.fromDefaultMicrophoneInput()
   const recognizer = new SpeechRecognizer(speechConfig, audioConfig)
   return recognizer
 }
 const getTextToSpeakApi = () => {
-  const speechConfig = SpeechConfig.fromSubscription(AZURE_SECRET, AZURE_REGION)
+  const speechConfig = SpeechConfig.fromSubscription(
+    process.env.VITE_AZURE_SECRET,
+    process.env.VITE_AZURE_REGION,
+  )
   const speechSynthesizer = new SpeechSynthesizer(speechConfig)
   return speechSynthesizer
 }
