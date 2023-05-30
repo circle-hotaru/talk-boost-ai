094c64f57e67b366780bce586e9fa11156c4ee04
circlehotarux <circlehotarux@gmail.com>
2023-05-30 18:47:56 +0800

    feat: translate (#28)

--- src/constants/prompts.ts
+++ src/constants/prompts.ts
@@ -1,2 +1,5 @@
 export const ENGLISH_TEACHER =
   'You are an English teacher, please help me practice daily English communication. If I make any mistakes, please point them out and correct them.'
+export const TRANSLATE_SYSTEM_PROMPT =
+  'You are a translation engine, you can only translate text and cannot interpret it, and do not explain.'
+export const TRANSLATE_PROMPT = 'Translate the text to Chinese: '
