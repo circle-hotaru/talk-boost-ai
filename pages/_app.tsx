5103e4313d61b239cf9fbb3be7fe31a230cb0822
circlehotarux <circlehotarux@gmail.com>
2023-07-19 23:31:56 +0800

    feat: add ga (#30)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,43 @@ import 'regenerator-runtime/runtime'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'jotai'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import * as gtag from 'gtag'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
-    <Provider>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${gtag.GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <Provider>
+        <Component {...pageProps} />
+      </Provider>
+    </>
   )
 }
 
