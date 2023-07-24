36548dccce3279451f93353d51d22b967aa4b44d
circlehotarux <circlehotarux@gmail.com>
2023-07-24 19:51:14 +0800

    fix: ga (#32)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,12 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
 
 export default function Document() {
+  const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID
   return (
     <Html lang="en">
       <Head>
         <Script
           strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+          src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
         ></Script>
         <Script
           id="gtag-init"
@@ -17,7 +18,7 @@ export default function Document() {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-MEASUREMENT_ID');
+              gtag('config', '${GOOGLE_ANALYTICS_ID}');
               `,
           }}
         />
