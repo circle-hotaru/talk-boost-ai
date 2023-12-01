5486acd65f7ad0f13d3a1acce412cd0d78fab93d
circlehotarux <circlehotarux@gmail.com>
2023-12-01 21:52:42 +0800

    chore: polish UI

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,19 @@
 import Link from 'next/link'
 
 const Footer: React.FC = () => {
+  const gradientText = {
+    fontWeight: 700,
+    background: 'linear-gradient(321deg,#00d1ff,#c059ff 46.3%,#f90)',
+    '-webkit-background-clip': 'text',
+    '-webkit-text-fill-color': 'transparent',
+  }
+
   return (
-    <div className='text-center text-gray-400'>
-      <p>
-        Made with ❤️ by{' '}
+    <div className='mb-4 text-center text-gray-400'>
+      <span>Made with</span>
+      <span style={gradientText}> Love </span>
+      <span>
+        by{' '}
         <Link className='font-bold' href='https://github.com/circle-hotaru'>
           circlehotarux
         </Link>{' '}
@@ -15,7 +24,7 @@ const Footer: React.FC = () => {
         >
           GitHub
         </Link>
-      </p>
+      </span>
     </div>
   )
 }
