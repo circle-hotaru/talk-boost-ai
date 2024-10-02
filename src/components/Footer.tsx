f8758376578727ddc6d86004346592827bd49aad
circlehotarux <circlehotarux@gmail.com>
2024-10-02 17:55:53 +0800

    chore: add tarot master link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,30 @@
+import Link from 'next/link'
+import { useTranslation } from 'react-i18next'
+import { ExternalLink } from 'lucide-react'
+
 const Footer: React.FC = () => {
+  const { t } = useTranslation()
   return (
-    <footer className='bg-white py-2 text-center text-sm text-gray-600 shadow-md'>
-      Made with love by{' '}
-      <a
-        href='https://github.com/circle-hotaru'
+    <footer className='flex flex-col items-center gap-2 bg-white py-2 text-sm text-gray-600 shadow-md'>
+      <Link
+        href='https://tarot.incircle.dev'
+        className='flex items-center hover:text-blue-500'
         target='_blank'
-        rel='noopener noreferrer'
-        className='font-medium text-blue-500'
       >
-        incircle
-      </a>
+        <span>{t('tarot_master_link')}</span>
+        <ExternalLink size={16} />
+      </Link>
+      <p className='mt-0'>
+        Made with ❤️ by{' '}
+        <Link
+          href='https://github.com/circle-hotaru'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='font-medium text-blue-500'
+        >
+          incircle
+        </Link>
+      </p>
     </footer>
   )
 }
